0bea02ce628a601c5a1dce1df7cf9f6a07c08af4
Crowdbotics <team@crowdbotics.com>
2024-02-26 16:38:57 +0000

    Initial commit

---
+++ backend/azure_demo3_46639/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for azure_demo3_46639 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "azure_demo3_46639.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "azure-demo3-46639",
+  url: "https://azure-demo3-46639.botics.co",
+  api: "https://azure-demo3-46639.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
